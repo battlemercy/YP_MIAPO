92020b699d77e87bcf44ed77280be21eb2531ea9
battlemercy <79182400+battlemercy@users.noreply.github.com>
2021-10-04 16:23:30 +0700

    EP

---
+++ BankTransactionsTests/Methods/M_AuthTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankTransactions.Methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTransactions.Methods.Tests
+{
+    [TestClass()]
+    public class M_AuthTests
+    {
+        M_Auth m_a = new M_Auth();
+        [TestMethod()]
+        public void Enter_Success()
+        {
+            bool actual = true,
+                 expected = true;
+
+            string login = "admin",
+                   password = "admin";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Enter_Fail()
+        {
+            bool actual = false,
+                 expected = false;
+
+            string login = "admin",
+                   password = "b10";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Enter_Null_Fail()
+        {
+            bool actual = false,
+                 expected = false;
+
+            string login = "admin",
+                   password = "";
+            try
+            {
+                actual = m_a.Enter(login, password);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ BankTransactionsTests/Methods/M_CTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankTransactions.Methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTransactions.Methods.Tests
+{
+    [TestClass()]
+    public class M_CTests
+    {
+        M_C m_c = new M_C();
+        [TestMethod()]
+
+        public void Word_Write_Success()
+        {
+            bool actual = true,
+                 expected = true;
+
+            string Last_Name = "Оладушек";
+            try
+            {
+                actual = m_c.Word_Check(Last_Name);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Number_Write_Fail()
+        {
+            bool actual = false,
+                 expected = false;
+
+            string Last_Name = "50000";
+            try
+            {
+                actual = m_c.Word_Check(Last_Name);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Spec_Symbols_Write_Fail()
+        {
+            bool actual = false,
+                 expected = false;
+
+            string Last_Name = "!@#$&/*%";
+            try
+            {
+                actual = m_c.Word_Check(Last_Name);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void English_Write_Fail()
+        {
+            bool actual = false,
+                 expected = false;
+
+            string Last_Name = "Popov";
+            try
+            {
+                actual = m_c.Word_Check(Last_Name);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
